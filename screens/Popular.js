bce3d9758ac7fa434fb0eb94a0d0faac1e341015
Bia <82560900+BiatrizR@users.noreply.github.com>
2023-10-12 17:12:40 -0300

    Update Popular.js

--- screens/Popular.js
+++ screens/Popular.js
@@ -12,18 +12,52 @@ import { RFValue } from "react-native-responsive-fontsize";
 import Star from 'react-native-star-view';
 
 export default class PopularMoviesScreen extends Component {
+  
   constructor(props) {
     super(props);
+    this.state = {
+      data: [],
+      ngrok_url: ""
+    };
   }
 
-  render() {
+ 
+  componentDidMount() {
+    this.getData();
+  }
+
+  /*2. defina a função getData() aqui*/
+  getData = () =>{
+    const url = this.state.ngrok_url + "/popular_movies";
+    axios
+    .get(url)
+    .then(async (response) => {
+      this.setState({ data: response.data.data });
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+  }
+
+ // * 3. passaar cada item do array e converter em string
+  keyExtractor = (item, index) => index.toString();
+
+//4 exibir na tela com render item tirar o render
+  renderItem = ({item, index}) =>{ 
+  
     return (
       <View style={styles.container}>
         <ImageBackground
           source={require("../assets/bg.png")}
           style={{ flex: 1 }}
         >
-          
+
+          {/*Adicione o componente FlatList para mostrar os dados dos filmes populares abaixo*/}
+          <FlatList
+            data={data}
+            keyExtractor={this.keyExtractor}
+            renderItem={this.renderItems}
+          />
         </ImageBackground>
       </View>
     );
